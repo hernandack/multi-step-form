c13d3df32f05ff3bcbec81757fee27317e6d0043
hernandack <hernandack@gmail.com>
2025-01-24 05:04:23 +0700

    remove unused state actions of customer

--- src/stores/customer.ts
+++ src/stores/customer.ts
@@ -8,30 +8,4 @@ export const customer = {
   seat: '' as string,
   food: '' as string,
   allergies: '' as string,
-
-  // actions
-  updateFirstName: function(value: string) {
-    this.firstName = value
-  },
-  updateLastName: function(value: string) {
-    this.lastName = value
-  },
-  updateAge: function(value: number) {
-    this.age = value
-  },
-  updatePhone: function(value: string) {
-    this.phone = value
-  },
-  updateEmail: function(value: string) {
-    this.email = value
-  },
-  updateSeat: function(value: string) {
-    this.seat = value
-  },
-  updateFood: function(value: string) {
-    this.food = value
-  },
-  updateAllergies: function(value: string) {
-    this.allergies = value
-  },
 }
